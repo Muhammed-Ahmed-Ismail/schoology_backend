3bf78ce94741fa09dc7e5c2194df0f59480532c1
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-07-04 22:03:59 +0200

    before pull

--- controllers/meetingcontroller.js
+++ controllers/meetingcontroller.js
@@ -5,13 +5,14 @@ const {
     getMeetingByStudentId
 } = require("../services/meetingServices")
 
-const createMeeting = async (req, res) => {
+const createMeeting = async (req, res,next) => {
     console.log("create meetign service")
     try {
         let meeting = await createMeetingService(req.body)
         res.json(meeting)
     } catch (error) {
-        res.send(error)
+        error.status=400
+        next(error)
     }
 }
 
