335921e7da1135774fb8c7d96ec1f6a334f45884
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-07-07 17:24:45 +0200

    applying jwt auth

--- routes/testdb.js
+++ routes/testdb.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const {createUser} = require("../controllers/test/testdbcontroller")
 const router = express.Router();
+const passport= require('passport')
 
-router.post("/create-user",createUser)
+router.post("/create-user",passport.authenticate('jwt', { session: false }),createUser)
 
 module.exports = router
--- services/loginService.js
+++ services/loginService.js
@@ -1,26 +1,32 @@
-
 const jwt = require("jsonwebtoken");
 const teacherDto = require("../dtos/teacherDto")
 require("dotenv").config();
-
+const fs = require('fs')
+const PRIVAREKEY=fs.readFileSync(process.env.PRIVATEKEYPATH,'utf8')
 const getjwtToken = (user)=>{
-
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    // console.log(jwtSecretKey);
+    console.log('in webtoken');
     let data = {
       time: Date(),
       userId: user.id,
+      iat: Date.now()
     }
-    return token = jwt.sign(data, jwtSecretKey);
+   const token =jwt.sign(data, PRIVAREKEY,{ algorithm: 'RS256' });
+     console.log(token);
+     return token
     
 }
 
 const logInTeacher = async (teacher)=>{
-    const token = getjwtToken( await teacher.getUser())
+    const user = await teacher.getUser()
+    const token = getjwtToken(user)
     const classes = await teacher.getClasses()
     console.log(classes)
     const course = await teacher.getCourse()
     const response = {
         userId:teacher.id,
+        userName:user.name,
+        userType: "teacher",
         course,
         classes,
         token
