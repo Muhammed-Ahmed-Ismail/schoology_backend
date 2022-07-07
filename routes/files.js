233d4cf383c8f7bf0261614cbe63b642c8fd1a8d
Private <msaleh.abdelrahman@gmail.com>
2022-07-07 20:15:26 +0200

    download file route

--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,10 @@
-const {upload, getListFiles, getTeacherFiles, getOne} = require("../controllers/filesController");
+const {upload, getListFiles, getTeacherFiles, getOne, download} = require("../controllers/filesController");
 const express = require("express");
 const router = express.Router();
 
 router.post('/:id/upload', upload);
 router.get('/:id/allFiles', getListFiles)
+router.get('/:id/download', download)
 router.get('/:id/myFiles', getTeacherFiles)
 router.get('/:id/download/:name', getOne)
 
