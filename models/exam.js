fb5e88d5aada6c92aa5e44396020ec2f91007524
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-07-08 04:08:58 +0200

    modify exams response

--- models/exam.js
+++ models/exam.js
@@ -10,17 +10,17 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Exam.belongsTo(models.Course , { foreignKey:'courseId' , as :'course'}) 
+      Exam.belongsTo(models.Course , { foreignKey:'courseId' , as :'course'})
       // Exam.belongsToMany(models.Student, { through: 'StudentExam' , foreignKey:'examId'}); //automatic many to many 
       // Exam.belongsTo(models.StudentExam, ); //manual many to many //{foreignKey:'examId' , as :'exam'} ##removed
-      Exam.belongsTo(models.Class, {  foreignKey:'examId' , as:'exam'}); 
+      Exam.belongsTo(models.Class, {  foreignKey:'classId' , as:'class'});
       
     }
   }
   Exam.init({
     name: DataTypes.STRING,
     link: DataTypes.STRING,
-    date: DataTypes.DATE,
+    date: DataTypes.DATEONLY,
     courseId: DataTypes.INTEGER,
     classId: DataTypes.INTEGER
     //ClassId
