d2af3030c075176ebebce12794d9e3888af1cd31
Adham-Ahmed <adhamahmed1234321@gmail.com>
2022-07-11 16:25:11 +0200

    message sender aquired from request done

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,8 +1,19 @@
 const {create,listBySenderAndReciever
 } = require("../controllers/messageController.js")
+const  {validateCreateMessageRequest} = require("../middleware/requestValidators/messages/createMessageRequest")
+
 const express = require("express");
 const router = express.Router();
+const passport= require('passport')
 
-router.post("/create" , create)
+router.use(passport.authenticate('jwt', { session: false }))
+router.post("/create" , validateCreateMessageRequest , create)
 router.get("/reciever/:id" , listBySenderAndReciever) // get all messages between sender (jwt) and reciever
 module.exports = router
+
+
+//To Do
+//header -> authorization : bearer {token from login here } // use req.user  
+////notice this ^^ will be put in message router to extract sender ID , it will also be added before any router that need authorization
+
+//route to get certain student all exams scores
