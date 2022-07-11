ac54425da444e79536b77dafb48b925d786dc862
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-07-11 14:08:20 +0200

    add teacherId to exam and change date to date only instead of datetime datatype

--- routes/examRouter.js
+++ routes/examRouter.js
@@ -1,4 +1,4 @@
-const {create,list,save,listBycourseId,listByclassId , listStudentExamByExamId
+const {create,list,save,listBycourseId,listByclassId , listStudentExamByExamId,listByTeacherId
 } = require("../controllers/examController")
 const express = require("express");
 const router = express.Router();
@@ -8,6 +8,7 @@ router.get("/list" , list)
 router.get("/list/course/:id" , listBycourseId) //list all exams for one course
 router.get("/list/class/:id" , listByclassId) // list all exams for one class
 router.get("/list/scores/:id" , listStudentExamByExamId) //lists students with scores for certian exam
+router.get("/list/teacher/:id" , listByTeacherId) //lists students with teacher for certian exam
 router.post("/save" , save) // saves students score to the database given body -> { "link" : "google form url here" }
 
 module.exports = router
