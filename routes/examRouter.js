5bced1beab8de5764529b2f6e6236d86ca9fbc05
Adham-Ahmed <adhamahmed1234321@gmail.com>
2022-07-11 21:04:56 +0200

    minor change (adding route authorization to ListStuendExams)

--- routes/examRouter.js
+++ routes/examRouter.js
@@ -13,7 +13,7 @@ router.get("/list/course/:id" , listBycourseId) //list all exams for one course
 router.get("/list/class/:id" , listByclassId) // list all exams for one class
 router.get("/list/scores/:id" , listStudentExamByExamId) //lists students with scores for certian exam
 router.get("/list/teacher/:id" , listByTeacherId) //lists students with teacher for certian exam
-router.get("/my-exams" , listStudentExams) //lists all exams for certain student
+router.get("/my-exams" ,isStudent , listStudentExams) //lists all exams for certain student
 router.post("/save" , save) // saves students score to the database given body -> { "link" : "google form url here" }
 
 module.exports = router
