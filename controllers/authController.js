97774bba15b858771b921f6c7b3ecf0bf7304b84
Radwa Mostafa <radwamostafa922@gmail.com>
2022-07-13 18:38:07 +0200

    adding getAllUsers route + update user

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,16 +65,15 @@ exports.signup = async (req, res) => {
         teacher.addClass(classRoom)
       }
       if (teacher) res.json({ user, teacher })
-
     }
     if (req.body.roleId === 3) {
       const parent = await Parent.create({
         userId: user.id,
         studentId: req.body.studentId,
+        relation: req.body.relation,
       });
       if (parent) res.json({ user, parent })
     }
-    // if (user) return res.status(200).send(user);
   }
   catch (error) {
     res.status(500).send({ message: error.message });
@@ -139,3 +138,59 @@ exports.deactivateUser = async (req, res) => {
     return res.status(500).json(error.message);
   }
 };
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    const phoneChanged = req.body.phone && user.phone !== req.body.phone;
+    const emailChanged = req.body.email && user.email !== req.body.email;
+
+    if ((phoneChanged && await User.findOne({ where: { phone: req.body.phone } })) ||
+      (emailChanged && await User.findOne({ where: { email: req.body.email } }))) {
+      return res.status(400).send("User already exists");
+    }
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.encryptedPassword = await bcrypt.hash(req.body.password, salt);
+    }
+    // copy params to user and save
+    Object.assign(user, req.body);
+    await user.save();
+
+    if (req.body.roleId === 2) {
+      const student = await Student.findByPk(user.id)
+      Object.assign(student, req.body);
+      await student.save();
+      if (student) res.json({ user, student })
+    }
+    if (req.body.roleId === 1) {
+      const teacher = await Teacher.findByPk(user.id)
+      Object.assign(teacher, req.body);
+      await teacher.save();
+      for (const element of req.body.classes) {
+        let classRoom = await Class.findByPk(element)
+        teacher.classUpdate(classRoom)
+      }
+      if (teacher) res.json({ user, teacher })
+    }
+    if (req.body.roleId === 3) {
+      const parent = await Parent.findByPk(user.id)
+      Object.assign(parent, req.body);
+      await parent.save();
+      if (parent) res.json({ user, parent })
+    }
+  }
+  catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+exports.AllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users)
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+}
