5a93607695e9906a14af9c71a2926e3013faaba5
Private <msaleh.abdelrahman@gmail.com>
2022-07-17 13:31:36 +0200

    admin and teacher restriction on meetings

--- routes/meeting.js
+++ routes/meeting.js
@@ -4,12 +4,13 @@ const  {validateCreateMeetingRequest} = require("../middleware/requestValidators
 const express = require("express");
 const router = express.Router();
 const passport= require('passport')
+const {isAdmin, isTeacher} = require("../middleware/roleAuthorization/role");
 
 router.use(passport.authenticate('jwt', { session: false }))
-router.post("/create",validateCreateMeetingRequest,createMeeting)
+router.post("/create", isAdmin || isTeacher ,validateCreateMeetingRequest,createMeeting)
 router.get("/my-meetings/:id",getMyMeetings)
 router.get("/all-meetings/:id", getAllMeetings)
-router.put("/update/:id", updateMeeting)
-router.delete("/delete/:id", deleteMeeting)
+router.put("/update/:id", isAdmin, updateMeeting)
+router.delete("/delete/:id", isAdmin, deleteMeeting)
 
 module.exports = router
