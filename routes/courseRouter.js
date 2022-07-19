df9dfc592c3c090debab1196a304eec98ad345dd
Private <msaleh.abdelrahman@gmail.com>
2022-07-19 02:04:13 +0200

    adjusted and added auth to course routes

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -2,11 +2,15 @@ const controller = require("../controllers/courseController");
 const express = require("express");
 const router = express.Router();
 
-router.get('/', controller.getAll)
-router.get('/:id', controller.findByPk)
+const passport = require('passport')
+const {isAdmin} = require('../middleware/roleAuthorization/role')
+router.use(passport.authenticate('jwt', {session: false}))
+
+router.get('/get-all', controller.getAll)
+router.get('/get/:id', controller.findByPk)
 router.get('/my-course/:id', controller.getTeacherCourse)
-router.post('/', controller.createCourse);
-router.put('/:id', controller.updateCourse)
-router.delete('/:id', controller.deleteCourse)
+router.post('/create', isAdmin, controller.createCourse);
+router.put('update/:id', isAdmin, controller.updateCourse)
+router.delete('delete/:id', isAdmin, controller.deleteCourse)
 
 module.exports = router
