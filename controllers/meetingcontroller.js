3e5668ba0bb53664de48a088cc4bee3715945a3e
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-07-23 02:00:15 +0200

    modify admin get teacher classes and course

--- controllers/meetingcontroller.js
+++ controllers/meetingcontroller.js
@@ -8,7 +8,7 @@ const {
     getAllMeetingsByParentId, getMeetingByParentId, notifyUsersByMeetingUpdate, notifyUsersByMeetingDeletion
 } = require("../services/meetingServices")
 
-const {Meeting, Teacher, Class,Course} = require("../models")
+const {Meeting, Teacher, Class, Course,User} = require("../models")
 
 
 const createMeeting = async (req, res, next) => {
@@ -61,7 +61,18 @@ const getAllMeetings = async (req, res, next) => {
                         model: Course,
                         as: 'course',
                         attributes: ['name']
-                    }]
+                    },
+                        {
+                            model: Teacher,
+                            as: 'teacher',
+                            include:[{
+                                model:User,
+                                as:'user',
+                                attributes: ['name']
+                            }],
+
+                        }
+                    ]
                 }
             );
             res.json(meetings)
@@ -112,7 +123,7 @@ const deleteMeeting = async (req, res) => {
     }
 };
 
-const checkIsValidTime = async (req,res)=>{
+const checkIsValidTime = async (req, res) => {
     console.log(req.query);
     const data = req.query
     data.classId = parseInt(data.classId)
@@ -121,16 +132,16 @@ const checkIsValidTime = async (req,res)=>{
     const classroom = await Class.findByPk(data.classId)
     const teacher = await Teacher.findByPk(data.teacherId)
 
-    if (! await classroom.isThatValidMeeting(data.date_time, data.period))
+    if (!await classroom.isThatValidMeeting(data.date_time, data.period))
 
-        res.json({error:"That class has another meeting in the same time !!"})
+        res.json({error: "That class has another meeting in the same time !!"})
 
-    else if (! await teacher.isThatValidMeeting(data.date_time, data.period)) 
+    else if (!await teacher.isThatValidMeeting(data.date_time, data.period))
 
-        res.json({error:"Teacher has another meeting in the same time !!"})
+        res.json({error: "Teacher has another meeting in the same time !!"})
 
-      else  res.json({sucsess:"That time is availabel "}) 
+    else res.json({sucsess: "That time is availabel "})
 
 }
 
-module.exports = {createMeeting, getMyMeetings, getAllMeetings, updateMeeting, deleteMeeting,checkIsValidTime}
+module.exports = {createMeeting, getMyMeetings, getAllMeetings, updateMeeting, deleteMeeting, checkIsValidTime}
