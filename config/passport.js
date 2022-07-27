3b023674bf520b704a6fd79f02d0728e4d8dd203
Private <msaleh.abdelrahman@gmail.com>
2022-07-27 13:52:55 +0200

    refactored app, index and credintials

--- config/passport.js
+++ config/passport.js
@@ -1,43 +1,33 @@
-const JwtStrategy = require('passport-jwt').Strategy
+const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const fs = require('fs');
-const path = require('path');
 require("dotenv").config();
 
-const {User} =  require("../models")
+const {User} = require("../models");
 
 const PUB_KEY = fs.readFileSync(process.env.PUBLICKEYPATH, 'utf8');
-console.log(PUB_KEY);
 
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: PUB_KEY,
     algorithms: ['RS256']
-  };
+};
 
-
-  module.exports = (passport) => {
-    // The JWT payload is passed into the verify callback
-    passport.use(new JwtStrategy(options, function(jwt_payload, done) {
-
+module.exports = (passport) => {
+    // The JWT payload is passed into to verify callback
+    passport.use(new JwtStrategy(options, function (jwt_payload, done) {
         console.log(jwt_payload);
-
-        try{
-        
-        User.findByPk( jwt_payload.userId).then( (user)=> {
-            
-            if (user) {
-                // console.log(user);
-                return done(null, user);
-            } else {
-                return done(null, false);
-            }
-            
-        })
-    }
-        catch (err) {
-                return done(err,false)
+        try {
+            User.findByPk(jwt_payload.userId).then((user) => {
+                if (user) {
+                    // console.log(user);
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            })
+        } catch (err) {
+            return done(err, false)
         }
-        
     }));
 }
