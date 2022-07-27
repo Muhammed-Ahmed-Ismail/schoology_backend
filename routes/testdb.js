cb4738840030ab5805abbbb2286699e51db60b36
Private <msaleh.abdelrahman@gmail.com>
2022-07-27 13:55:39 +0200

    removed test db files

--- routes/testdb.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {createUser} = require("../controllers/test/testdbcontroller")
-const router = express.Router();
-const passport= require('passport')
-
-router.post("/create-user",passport.authenticate('jwt', { session: false }),createUser)
-
-module.exports = router
