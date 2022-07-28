7f3118b414449698c67b0690347940920dcb079d
Private <msaleh.abdelrahman@gmail.com>
2022-07-29 01:21:11 +0200

    format, unify responses

--- models/file.js
+++ models/file.js
@@ -1,27 +1,28 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class File extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      File.belongsTo(models.Teacher,{foreignKey:'uploaderId',as:'teacher'})
-      File.belongsTo(models.Class,{foreignKey:'classId',as:'class'})
+    class File extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            File.belongsTo(models.Teacher, {foreignKey: 'uploaderId', as: 'teacher'})
+            File.belongsTo(models.Class, {foreignKey: 'classId', as: 'class'})
+        }
     }
-  }
-  File.init({
-    uploaderId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    classId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'File',
-  });
-  return File;
+
+    File.init({
+        uploaderId: DataTypes.INTEGER,
+        name: DataTypes.STRING,
+        classId: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'File',
+    });
+    return File;
 };
--- services/filesService.js
+++ services/filesService.js
@@ -1,109 +1,105 @@
 const util = require('util');
 const multer = require('multer');
 const path = require('path');
-const {File,Teacher,Class,User,Course} = require('../models')
-const {where} = require("sequelize");
+const {File, Teacher, Class, User, Course} = require('../models');
 const {sendNotificationToClass} = require("./Notifications");
 
-const filesPath = path.join(__dirname, '..', 'resources', 'static', 'uploads')
+const filesPath = path.join(__dirname, '..', 'resources', 'static', 'uploads');
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, filesPath);
     },
-    filename:  async (req, file, cb) => {
-        let teacher =   await req.user.getTeacher()
-        let classRoom = await Class.findByPk(req.body.classId)
+    filename: async (req, file, cb) => {
+        let teacher = await req.user.getTeacher();
+        console.log('req body', {...req.body})
+        let classRoom = await Class.findByPk(req.body.classId);
         await File.create({
             uploaderId: teacher.id,
-            name: classRoom.name+': '+file.originalname,
+            name: classRoom.name + ': ' + file.originalname,
             classId: req.body.classId
-        })
-        console.log('file name : ' + file.originalname);
-        cb(null, classRoom.name+': '+file.originalname);
-        await sendNotificationToClass(req.params.id, req.body.classId, 'a new file has been uploaded check it out')
-
-    },
-    //doesnt work
-    onFileUploadData: (file, data, req, res) => {
-        console.log('from upload data')
-    },
-    onFileUploadComplete: file => {
-        console.log('from complete')
+        });
+        console.log('file name : ' + file.originalname)
+        cb(null, classRoom.name + ': ' + file.originalname);
+        await sendNotificationToClass(req.params.id, req.body.classId,
+            'a new file has been uploaded check it out');
     }
 });
 
 const upload = multer({
     storage: storage,
-}).array('files', 1)
+}).array('files', 1);
 
 const getFilesById = async (id) => {
     try {
-        let files = await File.findAll({where: {id}})
-        return files
-    } catch (e) {
-        throw e
+        let files = await File.findAll({where: {id}});
+        return files;
+    } catch (error) {
+        throw error;
     }
 }
 const getFilesByUploaderId = async (teacher) => {
     try {
         const files = await teacher.getFiles({
-            include:[{
-                model:Class,
-                as:"class",
-                attributes:['name']
+            include: [{
+                model: Class,
+                as: "class",
+                attributes: ['name']
             }]
-        })
-        return files
-    } catch (e) {
-        throw e
+        });
+        return files;
+    } catch (error) {
+        throw error
     }
-
 }
+
 const getFilesByClassId = async (classRoom) => {
     try {
         const files = await classRoom.getFiles({
             include: [
                 {
                     model: Teacher,
-                    as:'teacher',
-                    include:[{
-                        model:User,
-                        as:'user',
-                        attributes:['name']
-                    },{
-                        model: Course,
-                        as:'course',
-                        attributes: ['name']
-                    }],
-                    attributes:['id']
+                    as: 'teacher',
+                    include: [
+                        {
+                            model: User,
+                            as: 'user',
+                            attributes: ['name']
+                        },
+                        {
+                            model: Course,
+                            as: 'course',
+                            attributes: ['name']
+                        }],
+                    attributes: ['id']
                 }
             ]
-        })
-        return files
-    } catch (e) {
-        throw e
+        });
+        return files;
+    } catch (error) {
+        throw error;
     }
 }
+
 const getFileByName = async (name) => {
     try {
-        let file = await File.findOne({where: {name}})
+        let file = await File.findOne({where: {name}});
         return file;
-    } catch (e) {
-        throw e;
+    } catch (error) {
+        throw error;
     }
 }
 
 const getAllFiles = async () => {
     try {
-        let files = await File.findAll()
-        return files
-    } catch (e) {
-        throw e
+        let files = await File.findAll();
+        return files;
+    } catch (error) {
+        throw error;
     }
 }
 
-let uploadService = util.promisify(upload)
+let uploadService = util.promisify(upload);
 
 module.exports = {
     uploadService,
